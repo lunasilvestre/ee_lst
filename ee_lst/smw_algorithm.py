f815a4b5f8df8a6490aab31a927fb710b4a7e44f
Nelson Luna Silvestre <lunasilvestre@mailbox.org>
2023-09-10 10:14:29 +0000

    Housekeeping and reverting smw_algorithm.py

--- ee_lst/smw_algorithm.py
+++ ee_lst/smw_algorithm.py
@@ -1,6 +1,14 @@
 import ee
 from ee_lst.constants import SMW_COEFFICIENTS, LANDSAT_BANDS
 
+def get_lookup_table(coeff, prop_1, prop_2):
+    """
+    Create a lookup between two columns in a list of dictionaries.
+    """
+    prop_1_list = [item[prop_1] for item in coeff]
+    prop_2_list = [item[prop_2] for item in coeff]
+    return prop_1_list, prop_2_list
+
 def add_lst_band(landsat, image):
     """
     Apply the Statistical Mono-Window algorithm to compute the LST.
@@ -13,28 +21,23 @@ def add_lst_band(landsat, image):
     - ee.Image: Image with added LST band
     """
     
-    # Check for valid Landsat version
-    if landsat not in LANDSAT_BANDS:
-        raise ValueError(f"Invalid Landsat version provided: {landsat}. Supported versions are: {', '.join(LANDSAT_BANDS.keys())}.")
+    # Select algorithm coefficients
+    coeff_SMW = SMW_COEFFICIENTS.get(landsat, SMW_COEFFICIENTS['L9'])  # Default to L9 if not found
     
-    # Check for required image bands
-    required_bands = ['EM', 'TPW'] + LANDSAT_BANDS[landsat]['TIR']
-    missing_bands = [band for band in required_bands if band not in image.bandNames().getInfo()]
-    if missing_bands:
-        raise ValueError(f"Missing required bands in the image: {', '.join(missing_bands)}.")
-
-    # Directly access the coefficients for the given Landsat version
-    coeff = SMW_COEFFICIENTS.get(landsat)
-    if not coeff:
-        raise ValueError(f"Missing coefficients for Landsat version: {landsat}.")
+    # Create lookups for the algorithm coefficients
+    A_lookup = get_lookup_table(coeff_SMW, 'TPWpos', 'A')
+    B_lookup = get_lookup_table(coeff_SMW, 'TPWpos', 'B')
+    C_lookup = get_lookup_table(coeff_SMW, 'TPWpos', 'C')
+  
+    # Map coefficients to the image using the TPW bin position
+    A_img = image.remap(A_lookup[0], A_lookup[1], 0.0, 'TPWpos').resample('bilinear')
+    B_img = image.remap(B_lookup[0], B_lookup[1], 0.0, 'TPWpos').resample('bilinear')
+    C_img = image.remap(C_lookup[0], C_lookup[1], 0.0, 'TPWpos').resample('bilinear')
     
-    A_img = ee.Image.constant(coeff['A'])
-    B_img = ee.Image.constant(coeff['B'])
-    C_img = ee.Image.constant(coeff['C'])
-    
-    # Directly access the TIR band from the LANDSAT_BANDS dictionary
+    # Select TIR band
     tir = LANDSAT_BANDS[landsat]['TIR'][0]
     
+    # Compute the LST
     lst = image.expression(
         'A * Tb1 / em1 + B / em1 + C',
         {
